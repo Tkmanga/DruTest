1c56c61122f6c2f6c14c7fed5dfba59d8262fcf7
dario <tacachojose@gmail.com>
2021-11-16 21:18:40 -0300

    config database

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const app = express()
+const sequelize = require('./database/db')
+//Setting 
 const PORT = process.env.PORT || 3000; 
-
-app.get('/', () => {
-    res.send('Hello World')
+//Routes
+app.get('/',  (req, res) => {
+    res.send('Hello World!')
 })
-
+//Start 
 app.listen(PORT, () => {
     console.log(`DruTest app listening on port ${PORT}`); 
+    // Database conection 
+    sequelize.authenticate().then(() => {
+        console.log(`We're conect to database`)
+    })
+    .catch(err =>{
+        console.log(`error: ${err}`)
+    })
 })
